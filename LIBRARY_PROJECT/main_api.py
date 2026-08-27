ca198d91b39994461e77d35032eb07bbe7540f2a
Ali Hassnain <ali.hassnain0365@gmail.com>
2026-08-27 21:52:52 +0500

    setted up the api for my library project to get, create and validate the books with author, settted up foreign key constraints

--- LIBRARY_PROJECT/main_api.py
+++ LIBRARY_PROJECT/main_api.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Depends, HTTPException
-from database import engine, sessionLocal
+from database import Session, sessionLocal
 from models import Author, Book
 from pydantic import BaseModel
+from sqlalchemy.exc import IntegrityError
+
 
 app = FastAPI()
 
@@ -17,6 +19,17 @@ class AuthorOut(BaseModel):
     name: str
     age: int
 
+class BookCreate(BaseModel):
+    title: str
+    author_id: int
+
+class BookOut(BaseModel):
+    id: int
+    title: str
+    author_id:int
+
+"""1. getting author """
+
 @app.get("/get_author/{author_id}", response_model=AuthorOut)
 def get_author(author_id: int, db= Depends(get_db)):
     author = db.get(Author, author_id)
@@ -24,3 +37,34 @@ def get_author(author_id: int, db= Depends(get_db)):
         raise HTTPException(status_code=404, detail="Id does not exists.")
     else:
         return author
+
+"""2. posting a book"""
+
+@app.post("/book/create_book", response_model=BookOut)
+def create_book(book: BookCreate, db:Session = Depends(get_db)):
+    try:
+        new_book = Book(
+            title = book.title,
+            author_id = book.author_id
+        )
+        db.add(new_book)
+        db.commit()
+        return new_book
+    except IntegrityError as e:
+        print(f"The following error happened", e)
+        raise HTTPException(status_code=400, detail="Invalid aturho_id")
+    
+
+"""3. getting book"""
+
+@app.get("/book/get_book/{book_id}", response_model=BookOut)
+def get_book(book_id: int, db:Session = Depends(get_db)):
+    book = db.get(Book,book_id)
+    if book is None:
+        raise HTTPException(status_code=404, detail="Book with this id not exists.")
+    else:
+        return book    
+
+    
+
+
